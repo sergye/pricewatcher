6c95b5a0d4a5e2525dd5d01a0d036d28d428cb9c
sergye <sergye@gmail.com>
2024-09-04 15:36:33 +0200

    Add brand model

---
+++ src/main/java/pricewatcher/app/model/Brand.java
@@ -0,0 +1,40 @@
+package pricewatcher.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "brands")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Brand {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private long id;
+
+	@Column(unique = true)
+	private String name;
+
+	@CreatedDate
+	private LocalDate createdAt;
+
+	@LastModifiedDate
+	private LocalDate updatedAt;
+}
