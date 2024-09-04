2487bf7deb44ed81404859630c93b17a5bb4ceee
sergye <sergye@gmail.com>
2024-09-04 16:10:37 +0200

    Add ResourceNotFoundException

---
+++ src/main/java/pricewatcher/app/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package pricewatcher.app.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
