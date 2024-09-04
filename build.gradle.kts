35c82ee7c4ec64490862b80d9d920c0054c1f0b5
sergye <sergye@gmail.com>
2024-09-04 15:15:21 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	runtimeOnly("com.h2database:h2")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
