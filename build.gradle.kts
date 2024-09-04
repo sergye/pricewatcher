561efa66d1129d45db4930f35b5a2ff0d88ab5f8
sergye <sergye@gmail.com>
2024-09-04 15:45:50 +0200

    Add faker

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("net.datafaker:datafaker:2.0.1")
+	implementation("org.instancio:instancio-junit:3.3.0")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+
 	runtimeOnly("com.h2database:h2")
+
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
