47d3cd1ff403502563c191520ecbc8c9e875cfcf
sergye <sergye@gmail.com>
2024-09-04 17:26:02 +0200

    Add BaseEntity

--- src/main/java/pricewatcher/app/mapper/ReferenceMapper.java
+++ src/main/java/pricewatcher/app/mapper/ReferenceMapper.java
@@ -1,6 +1,5 @@
 package pricewatcher.app.mapper;
 
-import pricewatcher.app.model.BaseEntity;
 import jakarta.persistence.EntityManager;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
@@ -14,7 +13,7 @@ public class ReferenceMapper {
     @Autowired
     private EntityManager entityManager;
 
-    public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) {
+    public <T> T toEntity(Long id, @TargetType Class<T> entityClass) {
         return id != null ? entityManager.find(entityClass, id) : null;
     }
 }
---
+++ src/main/java/pricewatcher/app/model/BaseEntity.java
@@ -0,0 +1,4 @@
+package pricewatcher.app.model;
+
+public interface BaseEntity {
+}
