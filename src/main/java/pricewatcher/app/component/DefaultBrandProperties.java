c307a0e790d8210902f5cb858ed7454a026a4681
sergye <sergye@gmail.com>
2024-09-05 16:58:54 +0200

    Add DefaultBrandProperties

---
+++ src/main/java/pricewatcher/app/component/DefaultBrandProperties.java
@@ -0,0 +1,13 @@
+package pricewatcher.app.component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class DefaultBrandProperties {
+    private String name = "Zara";
+}
