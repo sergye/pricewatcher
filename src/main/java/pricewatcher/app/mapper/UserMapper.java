07ce9e5c6a9a1ffee88ec18acd4ea0b611a5f3dd
sergye <sergye@gmail.com>
2024-09-07 19:18:21 +0200

    Add UserMapper

---
+++ src/main/java/pricewatcher/app/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package pricewatcher.app.mapper;
+
+import pricewatcher.app.dto.user.UserCreateDTO;
+import pricewatcher.app.dto.user.UserDTO;
+import pricewatcher.app.dto.user.UserUpdateDTO;
+import pricewatcher.app.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        uses = { JsonNullableMapper.class, ReferenceMapper.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class UserMapper {
+    public abstract User map(UserCreateDTO dto);
+    public abstract UserDTO map(User model);
+    public abstract void update(UserUpdateDTO dto, @MappingTarget User model);
+}
