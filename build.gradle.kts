429f3f8509c3dfd66057ffb113e16a66e4e3764d
sergye <sergye@gmail.com>
2024-09-07 18:46:30 +0200

    Add Spring Security

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,66 +1,69 @@
 plugins {
-	application
-	checkstyle
-	jacoco
-	id("org.springframework.boot") version "3.3.3"
-	id("io.spring.dependency-management") version "1.1.6"
-	id("com.github.ben-manes.versions") version "0.49.0"
-	id("io.freefair.lombok") version "8.6"
+    application
+    checkstyle
+    jacoco
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("com.github.ben-manes.versions") version "0.49.0"
+    id("io.freefair.lombok") version "8.6"
 }
 
 group = "pricewatcher"
 version = "0.0.1-SNAPSHOT"
 
 application {
-	mainClass.set("pricewatcher.app.Application")
+    mainClass.set("pricewatcher.app.Application")
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("net.datafaker:datafaker:2.0.1")
-	implementation("org.instancio:instancio-junit:3.3.0")
-	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-	implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("net.datafaker:datafaker:2.0.1")
+    implementation("org.instancio:instancio-junit:3.3.0")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
 
-	runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.h2database:h2")
 
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.test {
-	finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.jacocoTestReport)
 }
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
+    dependsOn(tasks.test)
 }
 
 tasks.jacocoTestReport {
-	reports {
-		xml.required = true
-	}
+    reports {
+        xml.required = true
+    }
 }
