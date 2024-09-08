b29ab2119a4909a7fd9734cce9472617b27141f9
sergye <sergye@gmail.com>
2024-09-08 10:08:29 +0200

    Refactor DataInitializer with default user

--- src/main/java/pricewatcher/app/component/DataInitializer.java
+++ src/main/java/pricewatcher/app/component/DataInitializer.java
@@ -1,44 +1,81 @@
 package pricewatcher.app.component;
 
 import pricewatcher.app.dto.brand.BrandCreateDTO;
+import pricewatcher.app.dto.user.UserCreateDTO;
 import pricewatcher.app.mapper.BrandMapper;
+import pricewatcher.app.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-
-import pricewatcher.app.model.Brand;
+import pricewatcher.app.model.User;
 import pricewatcher.app.repository.BrandRepository;
+import pricewatcher.app.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
 
 @Component
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
+
     @Autowired
-    private final BrandRepository brandRepository;
+    private BrandRepository brandRepository;
 
     @Autowired
     private BrandMapper brandMapper;
 
     @Autowired
-    private DefaultBrandProperties defaultBrandProperties;
+    private final UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DefaultUserProperties defaultUserProperties;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        createDefaultBrand();
+        createDefaultUser();
+        createDefaultBrands();
     }
 
-    private void createDefaultBrand() {
-        if (brandRepository.findAll().isEmpty()) {
-            String name = defaultBrandProperties.getName();
-            if (brandRepository.findByName(name).isEmpty()) {
-                BrandCreateDTO brandData = new BrandCreateDTO();
-                brandData.setName(name);
-                Brand brand = brandMapper.map(brandData);
-                brandRepository.save(brand);
+    private void createDefaultUser() {
+        if (userRepository.findAll().isEmpty()) {
+            String email = defaultUserProperties.getEmail();
+
+            if (userRepository.findByEmail(email).isEmpty()) {
+                UserCreateDTO userData = new UserCreateDTO();
+
+                userData.setFirstName("Jorge");
+                userData.setLastName("Lorenzo");
+                userData.setEmail(email);
+
+                String password = defaultUserProperties.getPassword();
+                String passwordDigest = passwordEncoder.encode(password);
+
+                userData.setPassword(passwordDigest);
+
+                User user = userMapper.map(userData);
+                userRepository.save(user);
             }
+        }
+    }
+
+    private void createDefaultBrands() {
+        if (brandRepository.findAll().isEmpty()) {
+            BrandCreateDTO zaraBrandDTO = new BrandCreateDTO();
+            zaraBrandDTO.setName("Zara");
+
+            BrandCreateDTO mangoBrandDTO = new BrandCreateDTO();
+            mangoBrandDTO.setName("Mango");
+
+            brandRepository.save(brandMapper.map(zaraBrandDTO));
+            brandRepository.save(brandMapper.map(mangoBrandDTO));
         }
     }
 }
