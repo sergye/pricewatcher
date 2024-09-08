1c0a46ecb1d126dee4fac61747f4e41ba67d67ab
sergye <sergye@gmail.com>
2024-09-08 10:23:01 +0200

    Refactor ModelGenerator with user model

--- src/main/java/pricewatcher/app/util/ModelGenerator.java
+++ src/main/java/pricewatcher/app/util/ModelGenerator.java
@@ -8,16 +8,26 @@ import org.instancio.Instancio;
 import org.instancio.Model;
 import org.instancio.Select;
 import org.springframework.stereotype.Component;
+import pricewatcher.app.model.User;
 
 @Getter
 @Component
 public class ModelGenerator {
     private static final Faker FAKER = new Faker();
 
+    private Model<User> userModel;
     private Model<Brand> brandModel;
 
     @PostConstruct
     private void init() {
+        userModel = Instancio.of(User.class)
+                .ignore(Select.field(User::getId))
+                .supply(Select.field(User::getFirstName), () -> FAKER.name().firstName())
+                .supply(Select.field(User::getLastName), () -> FAKER.name().lastName())
+                .supply(Select.field(User::getEmail), () -> FAKER.internet().emailAddress())
+                .supply(Select.field(User::getPassword), () -> FAKER.internet().password(5, 30))
+                .toModel();
+
         brandModel = Instancio.of(Brand.class)
                 .ignore(Select.field(Brand::getId))
                 .supply(Select.field(Brand::getName), () -> FAKER.name().name())
