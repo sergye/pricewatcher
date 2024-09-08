6a795a14bd555078018b76500e6f2934f65a79bb
sergye <sergye@gmail.com>
2024-09-08 10:31:12 +0200

    Refactor BrandControllerTest

--- src/main/java/pricewatcher/app/dto/brand/BrandUpdateDTO.java
+++ src/main/java/pricewatcher/app/dto/brand/BrandUpdateDTO.java
@@ -2,9 +2,10 @@ package pricewatcher.app.dto.brand;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 @Getter
 @Setter
 public class BrandUpdateDTO {
-    private String name;
+    private JsonNullable<String> name;
 }
--- src/test/java/pricewatcher/app/controller/BrandControllerTest.java
+++ src/test/java/pricewatcher/app/controller/BrandControllerTest.java
@@ -1,39 +1,48 @@
 package pricewatcher.app.controller;
 
-import java.util.Optional;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import pricewatcher.app.component.DefaultBrandProperties;
+import pricewatcher.app.component.DefaultUserProperties;
 import pricewatcher.app.dto.brand.BrandUpdateDTO;
 import pricewatcher.app.exception.ResourceNotFoundException;
 import pricewatcher.app.model.Brand;
+import pricewatcher.app.model.User;
 import pricewatcher.app.repository.BrandRepository;
 import pricewatcher.app.util.ModelGenerator;
-
-import net.datafaker.Faker;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+
 @SpringBootTest
 @AutoConfigureMockMvc
-class ApplicationTest {
 
-    private static final Faker FAKER = new Faker();
+class BrandControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
 
     @Autowired
     private MockMvc mockMvc;
@@ -48,30 +57,35 @@ class ApplicationTest {
     private BrandRepository brandRepository;
 
     @Autowired
-    private DefaultBrandProperties defaultBrandProperties;
+    private DefaultUserProperties defaultUserProperties;
 
+    private JwtRequestPostProcessor token;
+
+    private User user;
     private Brand brand;
 
     @BeforeEach
     public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+                .apply(springSecurity())
+                .build();
+
+        token = jwt().jwt(builder -> builder.subject(defaultUserProperties.getEmail()));
         brand = Instancio.of(modelGenerator.getBrandModel()).create();
+        brandRepository.save(brand);
     }
 
-
     @Test
-    public void testWelcomePage() throws Exception {
-        var result = mockMvc.perform(get("/welcome"))
-                .andExpect(status().isOk())
+    public void testUnauthenticatedAccess() throws Exception {
+        mockMvc.perform(get("/api/brands"))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
                 .andReturn();
-
-        var body = result.getResponse().getContentAsString();
-        assertThat(body).contains("Welcome to Price Watcher!");
     }
 
     @Test
     public void testIndex() throws Exception {
-        brandRepository.save(brand);
-        var result = mockMvc.perform(get("/api/brands"))
+        var result = mockMvc.perform(get("/api/brands").with(token))
                 .andExpect(status().isOk())
                 .andReturn();
 
@@ -79,18 +93,17 @@ class ApplicationTest {
         assertThatJson(body).isArray();
     }
 
-
     @Test
     public void testShow() throws Exception {
-        brandRepository.save(brand);
 
-        var result = mockMvc.perform(get("/api/brands/" + brand.getId()))
+        var result = mockMvc.perform(get("/api/brands/" + brand.getId()).with(token))
                 .andExpect(status().isOk())
                 .andReturn();
 
         var body = result.getResponse().getContentAsString();
 
         assertThatJson(body).and(
+                a -> a.node("id").isEqualTo(brand.getId()),
                 a -> a.node("name").isEqualTo(brand.getName())
         );
     }
@@ -99,9 +112,12 @@ class ApplicationTest {
     public void testCreate() throws Exception {
         var brandData = Instancio.of(modelGenerator.getBrandModel()).create();
 
-        mockMvc.perform(post("/api/brands")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(om.writeValueAsString(brandData)))
+        var request = post("/api/brands")
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(brandData));
+
+        mockMvc.perform(request)
                 .andExpect(status().isCreated());
 
         var brand = brandRepository.findByName(brandData.getName())
@@ -112,12 +128,12 @@ class ApplicationTest {
 
     @Test
     public void testUpdate() throws Exception {
-        brandRepository.save(brand);
 
         BrandUpdateDTO brandData = new BrandUpdateDTO();
-        brandData.setName(FAKER.name().name());
+        brandData.setName(JsonNullable.of("brand-name"));
 
         var request = put("/api/brands/" + brand.getId())
+                .with(token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(om.writeValueAsString(brandData));
 
@@ -127,27 +143,21 @@ class ApplicationTest {
         var updatedBrand = brandRepository.findById(brand.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("Brand not found"));
 
-        assertThat(updatedBrand.getName()).isEqualTo((brandData.getName()));
+        assertThat(updatedBrand.getName()).isEqualTo((brandData.getName().get()));
     }
 
     @Test
     public void testDelete() throws Exception {
-        brandRepository.save(brand);
 
-        mockMvc.perform(delete("/api/brands/" + brand.getId()))
+        mockMvc.perform(delete("/api/brands/" + brand.getId()).with(token))
                 .andExpect(status().isNoContent());
 
         assertThat(brandRepository.findById(brand.getId())).isEmpty();
     }
 
     @Test
-    public void testCreateDefaultBrand() {
-        String name = defaultBrandProperties.getName();
-        Optional<Brand> brandOptional = brandRepository.findByName(name);
-
-        assertThat(brandOptional).isPresent();
-        Brand brand = brandOptional.get();
-
-        assertThat(brand.getName()).isEqualTo(defaultBrandProperties.getName());
+    public void testDefaultBrands() {
+        assertThat(brandRepository.findByName("feature")).isPresent();
+        assertThat(brandRepository.findByName("bug")).isPresent();
     }
 }
