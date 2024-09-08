87e08dce378696dd748953b0f1997536b6ed86f6
sergye <sergye@gmail.com>
2024-09-08 15:45:12 +0200

    Add Product model

---
+++ src/main/java/pricewatcher/app/model/Product.java
@@ -0,0 +1,38 @@
+package pricewatcher.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.SequenceGenerator;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "products")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@SequenceGenerator(name = "product_id_seq", initialValue = 35455, allocationSize = 100000)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_seq")
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @CreatedDate
+    private LocalDate createdAt;
+}
