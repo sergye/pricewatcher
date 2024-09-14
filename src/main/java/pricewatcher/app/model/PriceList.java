5eb0d3b33367e3c5652cf23a0951a87bf34d99a8
sergye <sergye@gmail.com>
2024-09-14 10:48:13 +0200

    Add PriceList model

---
+++ src/main/java/pricewatcher/app/model/PriceList.java
@@ -0,0 +1,36 @@
+package pricewatcher.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "price_lists")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class PriceList {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
