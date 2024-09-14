86e3c2e67da6582aafdbaa1e39178f1d2fbd2f06
sergye <sergye@gmail.com>
2024-09-14 16:16:15 +0200

    Add ResourceAlreadyExistsException

--- src/main/java/pricewatcher/app/handler/GlobalExceptionHandler.java
+++ src/main/java/pricewatcher/app/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import pricewatcher.app.exception.ResourceAlreadyExistsException;
 import pricewatcher.app.exception.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -14,5 +15,10 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<String> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 }
