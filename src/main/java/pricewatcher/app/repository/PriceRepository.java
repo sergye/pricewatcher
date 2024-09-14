c58ce3e163461732a5e1bd4fa71d89e0bf04a310
sergye <sergye@gmail.com>
2024-09-14 18:33:55 +0200

    Remove unnecessary methods

--- src/main/java/pricewatcher/app/repository/PriceRepository.java
+++ src/main/java/pricewatcher/app/repository/PriceRepository.java
@@ -1,18 +1,15 @@
 package pricewatcher.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import pricewatcher.app.model.Brand;
 import pricewatcher.app.model.Price;
 import pricewatcher.app.model.PriceList;
-import pricewatcher.app.model.Product;
 
 import java.util.Optional;
 
 @Repository
-public interface PriceRepository extends JpaRepository<Price, Long> {
-    Optional<Price> findByBrand(Brand brand);
-    Optional<Price> findByProduct(Product product);
+public interface PriceRepository extends JpaRepository<Price, Long>, JpaSpecificationExecutor<Price> {
     Optional<Price> findByPriceList(PriceList priceList);
 }
