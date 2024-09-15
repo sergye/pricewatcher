64add856211c57533a41fc5c8f43fb16c563ce96
sergye <sergye@gmail.com>
2024-09-15 21:43:03 +0200

    Change type

--- src/main/java/pricewatcher/app/dto/price/PriceFilterDTO.java
+++ src/main/java/pricewatcher/app/dto/price/PriceFilterDTO.java
@@ -3,13 +3,11 @@ package pricewatcher.app.dto.price;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Setter
 @Getter
 public class PriceFilterDTO {
     private String product;
     private String brand;
     private String priceList;
-    private LocalDateTime priceDate;
+    private String priceDate;
 }
