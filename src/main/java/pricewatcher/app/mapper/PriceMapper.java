73b3554f891c88d9c45371dc4565723f258d71f8
sergye <sergye@gmail.com>
2024-09-15 21:42:03 +0200

    Add ReferenceMapper

--- src/main/java/pricewatcher/app/mapper/PriceMapper.java
+++ src/main/java/pricewatcher/app/mapper/PriceMapper.java
@@ -13,7 +13,7 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(
-        uses = { JsonNullableMapper.class },
+        uses = { JsonNullableMapper.class, ReferenceMapper.class },
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
         componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.IGNORE
