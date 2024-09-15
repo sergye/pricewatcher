e5be67cccb49ea706e1388a96caed66f9c3a37c6
sergye <sergye@gmail.com>
2024-09-15 09:47:18 +0200

    Add InvalidInputDataException

---
+++ src/main/java/pricewatcher/app/exception/InvalidInputDataException.java
@@ -0,0 +1,7 @@
+package pricewatcher.app.exception;
+
+public class InvalidInputDataException extends RuntimeException {
+    public InvalidInputDataException(String message) {
+        super(message);
+    }
+}
--- src/main/java/pricewatcher/app/handler/GlobalExceptionHandler.java
+++ src/main/java/pricewatcher/app/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import pricewatcher.app.exception.InvalidInputDataException;
 import pricewatcher.app.exception.ResourceAlreadyExistsException;
 import pricewatcher.app.exception.ResourceNotFoundException;
 
@@ -20,5 +21,10 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public ResponseEntity<String> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidInputDataException.class)
+    public ResponseEntity<String> handleInvalidInputDataException(InvalidInputDataException ex) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(ex.getMessage());
     }
 }
